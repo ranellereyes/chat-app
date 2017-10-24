25451d12bb1b0d273c8f03732d6a1d3f24ffdfb3
Ranelle <ranelle.reyes@gmail.com>
2017-10-23 23:16:48 -0700

    Fix merge conflicts

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const io = require('socket.io-client')();
+  const socket = require('socket.io-client')();
   const ChatUI = require('./chatUI');
-  const chat = new ChatUI(socket);
 
   const GUI = new ChatUI(socket);
   window.g = GUI;
